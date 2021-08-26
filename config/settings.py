b27010061c628d45a11f0f880b119361ad22e850
soo4767 <soo4767@naver.com>
2021-08-26 19:19:48 +0900

    edit

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+# DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = False
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
